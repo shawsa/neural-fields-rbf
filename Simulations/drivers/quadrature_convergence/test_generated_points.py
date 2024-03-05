334852ebe969e0bf71cd1a11b5d8ded7dfae7a14
Sage Shaw <sage.b.shaw@gmail.com>
2024-03-05 15:17:05 -0700

    convergence plots

--- Simulations/drivers/quadrature_convergence/test_generated_points.py
+++ Simulations/drivers/quadrature_convergence/test_generated_points.py
@@ -27,12 +27,12 @@ a1, a2 = 1, 1
 b1, b2 = sym.Rational(1, 2), sym.Rational(1, 2)
 
 test_functions = [
-    sym.sin(10 * x) * sym.sin(10 * y) + 1,
-    sym.cos(10 * x) * sym.cos(7 * y) + 1,
-    sym.cos(2 * sym.pi * b1 + a1 * x + a2 * y),  # Genz 1
-    1 / (a1**-2 + (x - b1) ** 2) / (a2**-2 + (y - b2) ** 2),  # Genz 2
-    (1 + a1 * x + a2 * y) ** -3,  # Genz 3
-    # sym.exp(-(a1**2) * (x - b1) ** 2 - a2**2 * (y - b2) ** 2),  # Genz 4
+    (sym.sin(10 * x) * sym.sin(10 * y) + 1, "sines"),
+    (sym.cos(10 * x) * sym.cos(7 * y) + 1, "cosines"),
+    (sym.cos(2 * sym.pi * b1 + a1 * x + a2 * y), "Genz 1"),
+    (1 / (a1**-2 + (x - b1) ** 2) / (a2**-2 + (y - b2) ** 2), "Genz 2"),
+    ((1 + a1 * x + a2 * y) ** -3, "Genz 3"),
+    # (sym.exp(-(a1**2) * (x - b1) ** 2 - a2**2 * (y - b2) ** 2), "Genz 4"),
 ]
 exact_values = [
     float(sym.integrate(sym.integrate(sym_func, (x, 0, 1)), (y, 0, 1)))
@@ -47,7 +47,7 @@ class Result:
     poly_deg: int
 
 
-for sym_func, exact in zip(test_functions, exact_values):
+for (sym_func, func_str), exact in zip(test_functions, exact_values):
     func = sym.lambdify((x, y), sym_func)
     results = []
     for qf in pregens():
@@ -77,4 +77,4 @@ for sym_func, exact in zip(test_functions, exact_values):
     plt.ylabel("Relative Error")
     plt.xlabel("$N^{-1/2}$")
     plt.title(f"${sym.latex(sym_func)}$")
-    plt.savefig(MEDIA_PATH + str(sym_func).replace("/", "div") + ".png")
+    plt.savefig(MEDIA_PATH + func_str + ".png")
